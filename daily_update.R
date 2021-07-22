3520d1890b03603c7c74c425d8639f231e14c872
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-07-22 15:41:54 -0400

    Add daily_update.R file to top level

---
+++ daily_update.R
@@ -0,0 +1,49 @@
+devtools::load_all("C:/Users/Administrator/Documents/R/Contributor_Repos/COHHIO/hud.export")
+hud <- hud.export::hud_export$new("Looker.ini")
+directory <- "data"
+dates_env <- dates(hud, write = TRUE)
+increment("Importing raw HMIS data\n")
+#TODO Where does public_data come from
+# list.files(full.names = TRUE, "~/R/Contributor_Repos/COHHIO/COHHIO_HMIS/public_data") %>%
+#   purrr::walk(~{
+#     file.copy(.x, file.path("~/R/Contributor_Repos/COHHIO/COHHIOHMIS/data/public",basename(.x)))
+#   })
+# file.copy("~/R/Contributor_Repos/COHHIO/COHHIO_HMIS/Looker.ini", "~/R/Contributor_Repos/COHHIO/COHHIOHMIS/Looker.ini")
+# c("cli",
+#   "feather",
+#   "HMIS",
+#   "lubridate",
+#   "readxl",
+#   "rlang",
+#   "scales",
+#   "janitor",
+#   "devtools",
+#   "fs",
+#   "purrr",
+#   # "urbnmapr",
+#   "sf",
+#   # "choroplethrMaps",
+#   "plotly") %>% sort %>% cat(sep = ",\n")
+
+
+
+# cohorts <- function(env) {
+#   #load("")
+#   # Code  ----
+#   # Mon Sep 28 11:20:46 2020
+#   # save.image
+#   environment()
+# }
+#
+#
+# env$cohorts <- cohorts()
+# Veterans <- function(env) {
+#   parent.env(environment(env$cohorts))
+#   #load(")
+#   #Veterans
+#   #save.image(")
+#   environment()
+# }
+#
+#
+# Veterans(env$cohorts)
