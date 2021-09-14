44bdeedcf7cf4e2a3cb65c733271387c985fd7a2
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-09-14 13:50:54 -0400

    Fix `exportEnv` value in daily_update

--- daily_update.R
+++ daily_update.R
@@ -16,7 +16,7 @@ jobs::jobscript({
   devtools::load_all("../clarity.looker")
   cl_api$get_export(.write = TRUE) # only need to run once
   cl_api$get_folder_looks(cl_api$folders$`HUD Extras`, .write = TRUE, path = dirs$extras)
-}, exportEnv = FALSE)
+}, exportEnv = "")
 
 Rm_env <- dates()
 Rm_env <- guidance()
