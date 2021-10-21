a94f65f3ffc51f2676a6a597a1f8acb695f2c793
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-10-21 08:42:24 -0400

    Restore `left_join`

--- R/04_DataQuality_utils.R
+++ R/04_DataQuality_utils.R
@@ -1105,7 +1105,7 @@ if (is_app_env(app_env))
                   AgeAtEntry,
                   RelationshipToHoH,
                   DisablingCondition) %>%
-    dplyr::inner_join(
+    dplyr::left_join(
       smallDisabilities %>%
         dplyr::filter(IndefiniteAndImpairs == 1L),
       by = c("PersonalID", "EnrollmentID")
