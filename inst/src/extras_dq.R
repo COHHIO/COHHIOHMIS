1cce3cb6cad6880fadb11a3b8949925285f0a74f
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-10-27 14:12:25 -0400

    extra_dq is the file to update all dq dependencies

--- inst/src/extras_dq.R
+++ inst/src/extras_dq.R
@@ -8,3 +8,7 @@ Rm_env <- load_export()
 Rm_env <- client_counts()
 Rm_env <- cohorts()
 Rm_env <- data_quality()
+Rm_env$write_app_deps(objs = Rm_env$app_objs$RminorElevated, path = file.path("data", "db", "RminorElevated"), dep_nms = Rm_env$app_deps$RminorElevated)
+# Uses RminorElevated as the default
+Rm_env$dropbox_auth()
+Rm_env$deps_to_apps()
