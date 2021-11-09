677ce7aaedd2750b23deff32ecba66dea54e952a
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-11-09 11:06:06 -0500

    Extras_dq: needs app_env & clarity_api passed manually

--- inst/src/extras_dq.R
+++ inst/src/extras_dq.R
@@ -3,16 +3,16 @@ dirs <- clarity.looker::dirs
 guidance <- guidance
 # Use the HUD CSV from the UI until the Looker API is fixed
 dirs$export <- "data"
-cl_api$.__enclos_env__$self$dirs <- dirs
-cl_api$get_folder_looks(cl_api$folders$`HUD Extras`, .write = TRUE, path = dirs$extras)
+Rm_env$.__enclos_env__$self$dirs <- dirs
+Rm_env$get_folder_looks(cl_api, app_env = Rm_env$folders$`HUD Extras`, .write = TRUE, path = dirs$extras)
 Rm_env$gather_deps(guidance)
 Rm_env$gather_deps(dirs)
-Rm_env <- dates(app_env = cl_api)
-Rm_env <- load_export(app_env = cl_api)
-Rm_env <- client_counts(app_env = cl_api)
-Rm_env <- cohorts(app_env = cl_api)
-Rm_env <- bed_unit_utilization(app_env = cl_api)
-Rm_env <- data_quality(app_env = cl_api)
+Rm_env <- dates(clarity_api = cl_api, app_env = Rm_env)
+Rm_env <- load_export(clarity_api = cl_api, app_env = Rm_env)
+Rm_env <- client_counts(app_env = Rm_env)
+Rm_env <- cohorts(clarity_api = cl_api, app_env = Rm_env)
+Rm_env <- bed_unit_utilization(clarity_api = cl_api, app_env = Rm_env)
+Rm_env <- data_quality(clarity_api = cl_api, app_env = Rm_env)
 Rm_env$write_app_deps(objs = Rm_env$app_objs$RminorElevated, path = file.path("data", "db", "RminorElevated"), dep_nms = Rm_env$app_deps$RminorElevated)
 # Uses RminorElevated as the default
 # Rm_env$dropbox_auth()
