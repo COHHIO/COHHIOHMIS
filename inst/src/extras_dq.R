04cfe1fdb93920a58a43517f0c41495bbde9f15e
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-11-12 17:37:56 -0500

    Extras_dq  fixed (dates still errors for some reason)

--- inst/src/extras_dq.R
+++ inst/src/extras_dq.R
@@ -1,17 +1,11 @@
+cl_api$get_folder_looks(cl_api$folders$`HUD Extras`, .write = TRUE, path = dirs$extras)
 devtools::load_all()
-dirs <- clarity.looker::dirs
-guidance <- guidance
-# Use the HUD CSV from the UI until the Looker API is fixed
-dirs$export <- "data"
-Rm_env$.__enclos_env__$self$dirs <- dirs
-Rm_env$get_folder_looks(cl_api, app_env = Rm_env$folders$`HUD Extras`, .write = TRUE, path = dirs$extras)
-Rm_env$gather_deps(guidance)
-Rm_env$gather_deps(dirs)
 Rm_env <- dates(clarity_api = cl_api, app_env = Rm_env)
 Rm_env <- load_export(clarity_api = cl_api, app_env = Rm_env)
 Rm_env <- client_counts(app_env = Rm_env)
 Rm_env <- cohorts(clarity_api = cl_api, app_env = Rm_env)
 Rm_env <- bed_unit_utilization(clarity_api = cl_api, app_env = Rm_env)
+#Rm_env <- prioritization(clarity_api = cl_api, app_env = Rm_env)
 Rm_env <- data_quality(clarity_api = cl_api, app_env = Rm_env)
 Rm_env$write_app_deps(objs = Rm_env$app_objs$RminorElevated, path = file.path("data", "db", "RminorElevated"), dep_nms = Rm_env$app_deps$RminorElevated)
 # Uses RminorElevated as the default
