21ddf155444d08c3cbbbba12787df1cf82a0bc4a
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-03 09:53:21 -0500

    Create `valid_movein_max`
    Combine `valid_max` to compare moveins with entry dates

--- R/03_prioritization_utils.R
+++ R/03_prioritization_utils.R
@@ -62,14 +62,37 @@ min_na <- function(...) {
   x[idx,]
 }
 
-valid_max = function(var) {
-  out <- na.omit(var)
+#' @title Find a valid maximum from the input values, or if none is present, return the values
+#'
+#' @param x \code({numeric/real})
+#'
+#' @return Maximum value
+#' @export
+
+valid_max = function(x) {
+  out <- na.omit(x)
   if (UU::is_legit(out)) {
     out2 <- max(out)
     if (!is.infinite(out2))
       out <- out2
   } else {
-    out <- var
+    out <- x
   }
+  out
+}
+
+#' @title Return a valid Move-In Date
+#'
+#' @param MoveInDateAdjust \code{(Date)} See `Enrollment_add_Household`
+#' @param EntryDate \code{(Date)} Date of Entry for Enrollment
+#'
+#' @return
+#' @export
+
+valid_movein_max = function(MoveInDateAdjust, EntryDate) {
+  if ((valid_max(MoveInDateAdjust)[1] > valid_max(EntryDate)[1]) %|% FALSE)
+    out <- valid_max(MoveInDateAdjust)
+  else
+    out <- lubridate::NA_Date_
   out
 }
