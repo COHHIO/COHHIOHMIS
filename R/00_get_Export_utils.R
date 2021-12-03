13bc0dc8054fcf249a19a27fdf145215f7fe227a
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-02 21:56:03 -0500

    `Enrollment_add_Household`: change nested `if_else` to `case_when` for clarity
    All `MoveInDateAdjust` were NA so something was wrong here. Chained `if_else` were difficult to understand, so it has been changed to a `case_when`.
    Not sure why EntryDate was used as MoveInDateAdjust

--- R/00_get_Export_utils.R
+++ R/00_get_Export_utils.R
@@ -160,11 +160,9 @@ Enrollment_add_Household = function(Enrollment, Project, rm_dates, app_env = get
     dplyr::left_join(HHEntry, by = "HouseholdID") %>%
     dplyr::mutate(
       # Puts EntryDate as MoveInDate for projects that don't use a MoveInDate
-      MoveInDateAdjust = dplyr::if_else(
-        !is.na(HHMoveIn) & HHMoveIn <= ExitAdjust,
-        dplyr::if_else(EntryDate <= HHMoveIn,
-                       HHMoveIn, EntryDate),
-        NA_real_),
+      MoveInDateAdjust = dplyr::case_when(
+        !is.na(HHMoveIn) & HHMoveIn <= ExitAdjust & EntryDate <= HHMoveIn ~ HHMoveIn,
+        TRUE ~ NA_real_),
       EntryAdjust = dplyr::case_when(
         ProjectType %in% c(1, 2, 4, 8, 12) ~ EntryDate,
         ProjectType %in% c(3, 9, 13) &
