7496721b7936ca7edcf919403504ded827ca0ec4
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-14 21:27:29 -0500

    Add `dq_plot` rendering function for _DataQuality_plots_

--- R/04_DataQuality_plots_utils.R
+++ R/04_DataQuality_plots_utils.R
@@ -14,3 +14,32 @@ dqu_plot_theme_labs <- function(g, x = NULL, y = NULL) {
     ggplot2::scale_fill_viridis_c(direction = -1) +
     ggplot2::theme_minimal(base_size = 18)
 }
+
+dq_plot <- function(.data, filter_exp, groups = c("ProjectName", "ProjectID"), y_label = "Clients", select  = TRUE, x_label = "") {
+  p_data <- .data
+  if (!missing(filter_exp))
+    p_data <- p_data %>%
+      dplyr::filter(
+        !!rlang::enquo(filter_exp)
+      )
+  if (select)
+    p_data <- dplyr::select(p_data, PersonalID, ProjectID, ProjectName) %>%
+      unique()
+
+  p_data <- dplyr::group_by(p_data, !!!purrr::map(groups, rlang::sym)) %>%
+    dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+    dplyr::arrange(dplyr::desc(n)) |>
+    dplyr::mutate(hover = paste0(ProjectName, ":", ProjectID))
+
+  out <-
+    ggplot2::ggplot(
+      utils::head(p_data, 20L),
+      ggplot2::aes(
+        x = stats::reorder(hover, n),
+        y = n,
+        fill = n
+      )
+    ) |>
+    dqu_plot_theme_labs(x = x_label, y = y_label)
+  out
+}
