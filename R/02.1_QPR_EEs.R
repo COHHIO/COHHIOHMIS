acb66135f3af26b7ce14982601845fdb8865523e
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2021-12-28 20:45:03 -0500

    `qpr_ees`:  clean up

--- R/02.1_QPR_EEs.R
+++ R/02.1_QPR_EEs.R
@@ -54,119 +54,142 @@ qpr_ees <- function(
   project_enrollment_small <- project_small |>
     dplyr::left_join(enrollment_small, by = "ProjectID")
 
-  qpr_leavers <- project_enrollment_small %>%
-    dplyr::filter((!is.na(ExitDate) | ProjectType %in% c(3, 9, 12)) &
-             HMIS::served_between(., rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) &
-             RelationshipToHoH == 1) %>%
+  qpr_leavers <- project_enrollment_small |>
+    HMIS::served_between(rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) |>
+    dplyr::filter((!is.na(ExitDate) | ProjectType %in% c(3, 9, 12)) &  RelationshipToHoH == 1) |>
     dplyr::mutate(
       DestinationGroup = dplyr::case_when(
-        Destination %in% c(destinations$temp) ~ "Temporary",
-        Destination %in% c(destinations$perm) ~ "Permanent",
-        Destination %in% c(destinations$institutional) ~ "Institutional",
-        Destination %in% c(destinations$other) ~ "Other",
+        Destination %in% destinations$temp ~ "Temporary",
+        Destination %in% destinations$perm ~ "Permanent",
+        Destination %in% destinations$institutional ~ "Institutional",
+        Destination %in% destinations$other ~ "Other",
         is.na(Destination) ~ "Still in Program"
       ),
       DaysinProject = difftime(ExitAdjust, EntryDate, units = "days")
-    ) %>%
+    ) |>
     HMIS::stayed_between(rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) |>
     dplyr::arrange(ProjectName)
 
-  qpr_rrh_enterers <- project_enrollment_small %>%
-    dplyr::filter(ProjectType == 13 &
-             HMIS::entered_between(., rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) &
-             RelationshipToHoH == 1) %>%
+  qpr_rrh_enterers <- project_enrollment_small |>
+    HMIS::entered_between(rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) |>
+    dplyr::filter(ProjectType == 13 & RelationshipToHoH == 1) |>
     dplyr::mutate(
       DaysToHouse = difftime(MoveInDateAdjust, EntryDate, units = "days"),
       DaysinProject = difftime(ExitAdjust, EntryAdjust, units = "days")
     )
 
-  smallMainstreamBenefits <- IncomeBenefits %>%
+  smallMainstreamBenefits <- IncomeBenefits |>
     dplyr::select(InsuranceFromAnySource, BenefitsFromAnySource,
-           DataCollectionStage, EnrollmentID, InformationDate) %>%
-    dplyr::group_by(EnrollmentID) %>%
-    dplyr::slice(which.max(InformationDate)) %>% # most recent answer per Enrollment
+           DataCollectionStage, EnrollmentID, InformationDate) |>
+    dplyr::group_by(EnrollmentID) |>
+    dplyr::slice(which.max(InformationDate)) |> # most recent answer per Enrollment
     dplyr::ungroup()
 
 
-  qpr_benefits <- project_enrollment_small %>%
-    dplyr::filter(HMIS::exited_between(., rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) &
-             RelationshipToHoH == 1) %>%
-    dplyr::left_join(smallMainstreamBenefits, by = "EnrollmentID") %>%
-    dplyr::select(ProjectName, PersonalID, HouseholdID, EntryDate,
-           EntryAdjust, MoveInDate, MoveInDateAdjust, ExitDate, ExitAdjust,
-           InsuranceFromAnySource, BenefitsFromAnySource, DataCollectionStage,
-           InformationDate, ProjectRegion, ProjectCounty, ProjectType) %>%
-    dplyr::mutate(ProjectType = HMIS::hud_translations$`2.02.6 ProjectType`(ProjectType)) %>%
+  qpr_benefits <- project_enrollment_small |>
+    HMIS::exited_between(rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) |>
+    dplyr::filter(RelationshipToHoH == 1) |>
+    dplyr::left_join(smallMainstreamBenefits, by = "EnrollmentID") |>
+    dplyr::select(
+      ProjectName,
+      UniqueID,
+      PersonalID,
+      HouseholdID,
+      EntryDate,
+      EntryAdjust,
+      MoveInDate,
+      MoveInDateAdjust,
+      ExitDate,
+      ExitAdjust,
+      InsuranceFromAnySource,
+      BenefitsFromAnySource,
+      DataCollectionStage,
+      InformationDate,
+      ProjectRegion,
+      ProjectCounty,
+      ProjectType
+    ) |>
+    dplyr::mutate(ProjectType = HMIS::hud_translations$`2.02.6 ProjectType`(ProjectType)) |>
     dplyr::arrange(ProjectName, HouseholdID)
 
-  incomeMostRecent <- IncomeBenefits %>%
+  incomeMostRecent <- IncomeBenefits |>
     dplyr::select(IncomeFromAnySource, TotalMonthlyIncome, DataCollectionStage,
-           EnrollmentID, InformationDate) %>%
-    dplyr::group_by(EnrollmentID) %>%
-    dplyr::slice(which.max(InformationDate)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(RecentIncome = TotalMonthlyIncome) %>%
+           EnrollmentID, InformationDate) |>
+    dplyr::group_by(EnrollmentID) |>
+    dplyr::slice(which.max(InformationDate)) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(RecentIncome = TotalMonthlyIncome) |>
     dplyr::select(EnrollmentID, RecentIncome)
 
-  incomeAtEntry <- IncomeBenefits %>%
+  incomeAtEntry <- IncomeBenefits |>
     dplyr::select(IncomeFromAnySource, TotalMonthlyIncome, DataCollectionStage,
-           EnrollmentID, InformationDate) %>%
-    dplyr::group_by(EnrollmentID) %>%
-    dplyr::slice(which.min(InformationDate)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(EntryIncome = TotalMonthlyIncome) %>%
+           EnrollmentID, InformationDate) |>
+    dplyr::group_by(EnrollmentID) |>
+    dplyr::slice(which.min(InformationDate)) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(EntryIncome = TotalMonthlyIncome) |>
     dplyr::select(EnrollmentID, EntryIncome)
 
   smallIncomeDiff <-
     dplyr::full_join(incomeAtEntry, incomeMostRecent, by = "EnrollmentID")
 
-  qpr_income <- project_enrollment_small %>%
-    dplyr::filter(HMIS::served_between(., rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) &
-             RelationshipToHoH == 1) %>%
-    dplyr::left_join(smallIncomeDiff, by = "EnrollmentID") %>%
-    dplyr::select(ProjectName, PersonalID, HouseholdID, EntryDate,
-           EntryAdjust, MoveInDate, MoveInDateAdjust, ExitDate, ExitAdjust,
-           EntryIncome, RecentIncome, ProjectRegion, ProjectCounty, ProjectType) %>%
+  qpr_income <- project_enrollment_small |>
+    HMIS::served_between(rm_dates$calc$data_goes_back_to, rm_dates$meta_HUDCSV$Export_End) |>
+    dplyr::filter(RelationshipToHoH == 1) |>
+    dplyr::left_join(smallIncomeDiff, by = "EnrollmentID") |>
+    dplyr::select(
+      ProjectName,
+      UniqueID,
+      PersonalID,
+      HouseholdID,
+      EntryDate,
+      EntryAdjust,
+      MoveInDate,
+      MoveInDateAdjust,
+      ExitDate,
+      ExitAdjust,
+      EntryIncome,
+      RecentIncome,
+      ProjectRegion,
+      ProjectCounty,
+      ProjectType
+    ) |>
     dplyr::mutate(
       Difference = RecentIncome - EntryIncome,
       ProjectType = HMIS::hud_translations$`2.02.6 ProjectType`(ProjectType)
-    ) %>%
+    ) |>
     dplyr::arrange(ProjectName, HouseholdID)
 
-  qpr_spending <- Services_enroll_extras %>%
+  qpr_spending <- Services_enroll_extras |>
     dplyr::left_join(Enrollment_extra_Client_Exit_HH_CL_AaE,
-              by = c("EnrollmentID", "PersonalID")) %>%
-    dplyr::left_join(project_small, by = c("ProjectID", "ProjectType", "ProjectName")) %>%
+              by = UU::common_names(Services_enroll_extras, Enrollment_extra_Client_Exit_HH_CL_AaE)) |>
+    dplyr::left_join(project_small, by = c("ProjectID", "ProjectType", "ProjectName")) |>
     dplyr::select(
+      UniqueID,
       PersonalID,
       OrganizationName,
       ProjectName,
       ProjectRegion,
       ProjectType,
-      Amount,
-      Description,
+      ServiceAmount,
+      ServiceItemName,
       RelationshipToHoH,
       ServiceStartDate,
       EntryDate,
       MoveInDateAdjust,
       ExitDate
-    ) %>%
+    ) |>
     dplyr::filter(ProjectType %in% c(13, 12) &
              RelationshipToHoH == 1 &
-             !is.na(Amount)) %>%
+             !is.na(ServiceAmount)) |>
     dplyr::select(-RelationshipToHoH)
 
-  rm(enrollment_small,
-     project_small,
-     smallMainstreamBenefits,
-     incomeMostRecent,
-     incomeAtEntry,
-     smallIncomeDiff)
+
 
 
   # Save it out -------------------------------------------------------------
   # WARNING save.image does not save the environment properly, save must be used.
-  app_env$gather_deps("everything")
+  app_env$gather_deps(ls(pattern = "^qpr"))
 
 
 }
