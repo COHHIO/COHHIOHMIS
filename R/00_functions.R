45445c987df56daca36301a1bb4a21600592dee5
Trevin Flickinger <twflick@gmail.com>
2022-04-26 16:12:59 -0400

    change age years function
    function was showing people that were 18 as 17 year olds

--- R/00_functions.R
+++ R/00_functions.R
@@ -176,7 +176,7 @@ increment <- function(..., cenv = rlang::caller_env()) {
 
 age_years <- function(earlier, later)
 {
-  floor(lubridate::as.duration(later - earlier) / lubridate::dyears(1))
+  lubridate::time_length(lubridate::interval(earlier, later), "years")
 }
 
 #' @title Copy files to directory
